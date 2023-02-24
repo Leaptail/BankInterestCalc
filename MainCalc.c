bf6d0115ca416dc754c3836ef24076a9a76e2945
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-02-24 22:26:23 +0800

    fixed date calc

--- MainCalc.c
+++ MainCalc.c
@@ -4,7 +4,7 @@
 //function to get first 2 digits
 int First2digit(int Num)
 {
-    while(Num >= 100) Num/=10;
+    Num /= 1000000;
     return Num;
 }
 
@@ -27,14 +27,23 @@ int Yeareturn(int Num)
 int FDaydiff(int date1,int date2)
 {
 	int Months[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-	int days;
-	for(int h=Monthdigit(date1);h<=Monthdigit(date2);h++) days+=Months[h-1];
+	int days=0,daysbefore=0,daysafter=0;
+
+	//days in year before first date
+	for(int h=0;h<=Monthdigit(date1)-1;h++) daysbefore+=Months[h];
+	daysbefore+=First2digit(date1); 
+	
+	//days in year after second date
+	for(int h=Monthdigit(date2)-1;h<=11;h++) daysafter+=Months[h];//WHY TF DOES <= WORK AND NOT ==????????????????????
+	daysafter-=Months[Monthdigit(date2)]+First2digit(date2);
+
+	days=(365*(Yeareturn(date2)-Yeareturn(date1)+1))-daysbefore-daysafter;
 	return days;
 }
 
 int main(int argc, char *argv[])
 {
-	int liquid=0,inve=0,salary=0,date;
+	int liquid=0,inve=0,salary=0,date1;
     int liquid2=0,inve2=0,salary2=0,date2;
 	int daydiff;
 	double growth=0;
@@ -49,17 +58,18 @@ int main(int argc, char *argv[])
 	scanf("%d %d %d %d", &liquid2, &inve2, &salary2, &date2);
 	
     //read numbers in file and compare the interest rate
-	fscanf(in,"%d %d %d %d",&liquid,&inve,&salary,&date);
+	fscanf(in,"%d %d %d %d",&liquid,&inve,&salary,&date1);
     printf("Change in liq/inv/sal: \n             %d %d %d",liquid2-liquid,inve2-inve,salary2-salary);
-    
+	
 	//Calculating Date change
-	daydiff=FDaydiff(date,date2);
+	daydiff=FDaydiff(date1,date2);
 	printf("\nDifference in days: %4d", daydiff);
-
-	//make days since update!
 
 	//write numbers to file in the next row
 	
 	//other cool calculations i might want
+
+	//close all files
 	fclose(in);
+	//fclose(out);
 }
