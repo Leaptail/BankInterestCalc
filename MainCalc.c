c220b6e8e4520c410e552789e7655f6e5eb2b166
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-02-24 21:33:11 +0800

    New updates
    added month diff calc and print diff

--- MainCalc.c
+++ MainCalc.c
@@ -16,24 +16,50 @@ int Monthdigit(int Num)
     return Num;
 }
 
+//function to return the year
+int Yeareturn(int Num)
+{
+	Num %= 10000;
+	return Num;
+}
+
+//difference between dates in days
+int FDaydiff(int date1,int date2)
+{
+	int Months[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int days;
+	for(int h=Monthdigit(date1);h<=Monthdigit(date2);h++) days+=Months[h-1];
+	return days;
+}
+
 int main(int argc, char *argv[])
 {
-	int liquid=0,inve=0,salary=0, date;
+	int liquid=0,inve=0,salary=0,date;
     int liquid2=0,inve2=0,salary2=0,date2;
+	int daydiff;
 	double growth=0;
-	FILE * out = fopen("Bnk.txt","w");
+	//FILE * out = fopen("Bnk.txt","w");
 	FILE * in = fopen("Bnk.txt","r");
-	if (in==NULL||out==NULL) printf("ERROR Unable to open");
-	printf("Enter The numbers liquid/invest/salary/date: \n");
-	scanf("%d %d %d %d", &liquid, &inve, &salary, &date);
-	
-        //read numbers in file and compare the interest rate
-	fscanf(in,"%d %d %d %d",&liquid2,&inve2,&salary2,&date2);
-    printf("Change in liq/inv/sal: \n             %d %d %d",liquid2-liquid,inve2-inve,salary2-salary);
-    //now we need to take the first 2 numbers of the date (ddmmyyyy)
 
+	//check file exist ||out==NULL
+	if (in==NULL) printf("ERROR Unable to open");
+
+	//ask for new numbers
+	printf("Enter The numbers liquid/invest/salary/date: \n");
+	scanf("%d %d %d %d", &liquid2, &inve2, &salary2, &date2);
+	
+    //read numbers in file and compare the interest rate
+	fscanf(in,"%d %d %d %d",&liquid,&inve,&salary,&date);
+    printf("Change in liq/inv/sal: \n             %d %d %d",liquid2-liquid,inve2-inve,salary2-salary);
+    
+	//Calculating Date change
+	daydiff=FDaydiff(date,date2);
+	printf("\nDifference in days: %4d", daydiff);
+
+	//make days since update!
 
 	//write numbers to file in the next row
 	
 	//other cool calculations i might want
+	fclose(in);
 }
